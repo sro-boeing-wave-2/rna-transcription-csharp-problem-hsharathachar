98eb00bce8f320a2dd2e3b4c23e5c5e4a20e2558
H Sharath Achar <sharathachar110@gmail.com>
2018-07-19 22:01:04 +0530

    wrote the program

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -6,7 +6,19 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            //throw new NotImplementedException("You need to implement this function.");
+
+            nucleotide = nucleotide.Replace("G","temp1");
+            nucleotide = nucleotide.Replace("C","temp2");
+            nucleotide = nucleotide.Replace("T","temp3");
+            nucleotide = nucleotide.Replace("A","temp4");
+
+            nucleotide = nucleotide.Replace("temp1","C");
+            nucleotide = nucleotide.Replace("temp2","G");
+            nucleotide = nucleotide.Replace("temp3","A");
+            nucleotide = nucleotide.Replace("temp4","U");
+
+            return nucleotide;
         }
     }
 }
